4a1ea6ca1f8383ca5c9a7195fe0dfc1c9e5b2c82
priyadarshansathya <51115813+priyadarshansathya@users.noreply.github.com>
2021-07-31 15:41:17 +0530

    First Commit

---
+++ Scripts/5PsisaScanner.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 31 15:15:33 2021
+
+@author: Priyadarshan.Sathya
+"""
+
+from py5paisa import FivePaisaClient
+
+client = FivePaisaClient(email="priyadarsan17@yahoo.com", passwd="67BJsvs84612", dob="19960604")
+
+client.login()
