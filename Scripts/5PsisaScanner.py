3d4462284da4aa007835a5e01353c6fc39f2c61b
priyadarsan17 <priyadarsan17@gmail.com>
2021-07-31 18:15:18 +0530

    changes 1

--- Scripts/5PsisaScanner.py
+++ Scripts/5PsisaScanner.py
@@ -10,3 +10,9 @@ from py5paisa import FivePaisaClient
 client = FivePaisaClient(email="priyadarsan17@yahoo.com", passwd="67BJsvs84612", dob="19960604")
 
 client.login()
+
+req_list_=[{"Exch":"N","ExchType":"D","Symbol":"NIFTY 26 AUG 2021 CE 16000.00","Expiry":"20210826","StrikePrice":"16000","OptionType":"CE"},
+            {"Exch":"N","ExchType":"D","Symbol":"NIFTY 26 AUG 2021 PE 16000.00","Expiry":"20210826","StrikePrice":"16000","OptionType":"PE"}]
+            
+client.fetch_market_feed(req_list_)
+
